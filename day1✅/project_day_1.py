d495e36efa4329fb4dd9e3a4bf9f74c918573453
Ahmed2309 <ahmedmh23009@gmail.com>
2025-11-21 13:47:36 +0300

    This is day 1 project

---
+++ day1✅/project_day_1.py
@@ -0,0 +1,9 @@
+# this is way to do it
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
+
+# this is trying do it in one or two lines
+print("Welcome to the Band Name Generator.")
+print("Your band name could be " + input("What's name of the city you grew up in?\n") + " " + input("What's your pet's name?\n"))
